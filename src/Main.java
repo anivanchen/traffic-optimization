be10dfa796dde1f73b95c0b56ca3707b3a9a9992
TraceyLin <tlin41@stuy.edu>
2023-04-30 00:18:31 -0400

    make a loop in main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    private int t;
+    private ArrayList<Car> cars;
+    private ArrayList<TrafficSignal> stoplights;
+    private MyCanvas canvas;
+
+    public void main(String[] args) {
         // StdDraw.setPenColor(Color.WHITE);
         // StdDraw.filledRectangle(50, 100, 10, 20);
-        ArrayList<Car> cars = new ArrayList<Car>();
-        ArrayList<TrafficSignal> trafficSignals = new ArrayList<TrafficSignal>();
-        MyCanvas canvas = new MyCanvas(cars, trafficSignals);
+
         // canvas.setBackground(Color.BLACK);
         // StdDraw.setPenColor(Color.WHITE);
 
         // StdDraw.circle(5, 5, 10);
         // StdDraw.show();
-    }
+
+		long ms  = System.currentTimeMillis();
+		while (true) {  
+			if ((int)ms % 100 == 0) {
+                t += 1;
+                for(Car i : cars){
+                    i.update(System.currentTimeMillis()-ms);
+                }
+                for(TrafficSignal i : stoplights){
+                    i.getCurrentCycle();
+                }
+                canvas = new MyCanvas(cars, stoplights);
+            }
+			ms = System.currentTimeMillis();
+		}
+	}
 
 }
